315bf6f2d2371f2ccebc148189f13d4c5daa2de9
mahjoub933 <61935999+mahjoub933@users.noreply.github.com>
2021-08-03 22:27:48 +0000

    Add files via upload
    Mahjoub

---
+++ src/main/java/com/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetSpringBootCoreDataJpaMvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRestApplication.class, args);
+	}
+
+}
